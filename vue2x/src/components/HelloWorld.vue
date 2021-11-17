614cdc9a90f10b25f839238af20d2c2cf14f4ad4
Irina <irina2358@yandex.ru>
2021-11-17 21:21:17 +0300

    DZ2

--- vue2x/src/components/HelloWorld.vue
+++ vue2x/src/components/HelloWorld.vue
@@ -2,17 +2,57 @@
  <template>
   <div class="hello">
     <br />
-    <input type="number" placeholder="operand1" v-model.number="operand1" />
-    <input type="number" placeholder="operand2" v-model.number="operand2" />
+
+    <label for="one"
+      ><input
+        type="number"
+        placeholder="operand1"
+        v-model.number="operand1"
+        @focus="focusInput('operand1')" />
+      <input type="radio" id="one" value="operand1" v-model="selectedOperand"
+    /></label>
+
+    <label for="two"
+      ><input
+        type="number"
+        placeholder="operand2"
+        v-model.number="operand2"
+        @focus="focusInput('operand2')" />
+      <input type="radio" id="two" value="operand2" v-model="selectedOperand"
+    /></label>
+
     <br />
     {{ result }}
     <br />
+
     <button @click="calculate('+')">+</button>
     <button @click="calculate('-')">-</button>
     <button @click="calculate('*')">*</button>
     <button @click="calculate('/')">/</button>
     <button @click="calculate('exponentiation')">exponentiation</button>
     <button @click="calculate('integer_div')">integer division</button>
+
+    <div class="margin-top">
+      <button
+        v-show="show_keyboard"
+        class="keyboard"
+        @click="numberInput(btn.number)"
+        v-for="btn in btns"
+        :key="btn.number"
+      >
+        {{ btn.number }}
+      </button>
+      <button @click="del()">delete</button>
+    </div>
+    <div class="margin-top">
+      <input
+        type="checkbox"
+        id="checkbox"
+        v-model="checked"
+        @click="show_keyboard = !show_keyboard"
+      />
+      keyboard
+    </div>
   </div>
 </template>
 
@@ -28,11 +68,47 @@ export default {
       result: 0,
       operand1: 0,
       operand2: 0,
+      selectedOperand: 0,
+      checked: true,
+      show_keyboard: true,
+      btns: [
+        { number: 1 },
+        { number: 2 },
+        { number: 3 },
+        { number: 4 },
+        { number: 5 },
+        { number: 6 },
+        { number: 7 },
+        { number: 8 },
+        { number: 9 },
+        { number: 0 },
+      ],
     };
   },
   methods: {
     onChange(str, event) {
       console.log(str, event);
+    },
+    numberInput(number) {
+      if (this[this.selectedOperand] === 0) {
+        this[this.selectedOperand] = number;
+      } else
+        this[this.selectedOperand] = +`${this[this.selectedOperand]}${number}`;
+    },
+    del() {
+      let newArr = this[this.selectedOperand]
+        .toString()
+        .split("")
+        .slice(0, -1)
+        .join("");
+
+      this[this.selectedOperand] = `${newArr}`;
+      console.log(newArr);
+    },
+    focusInput(operand) {
+      this.selectedOperand = operand;
+      console.log(this.operand);
+      console.log(this.selectedOperand);
     },
     calculate(action = "+") {
       switch (action) {
@@ -107,5 +183,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.margin-top {
+  margin-top: 20px;
 }
 </style>
